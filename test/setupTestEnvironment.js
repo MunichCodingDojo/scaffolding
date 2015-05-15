ba9befd79887ac7efeaa34374e93d0eaf4b76373
Mathias Schreck <schreck.mathias@googlemail.com>
2015-05-15 17:26:53 +0200

    Upgrade to jsdom 4

--- test/setupTestEnvironment.js
+++ test/setupTestEnvironment.js
@@ -4,7 +4,7 @@ var jsdom = require('jsdom'),
     chai = require('chai');
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = document.parentWindow;
+global.window = document.defaultView;
 global.navigator = window.navigator;
 
 chai.use(require('chai-string'));
